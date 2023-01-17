4b90fa12ca8d74f3a7dc11c23c44334c72672b18
stricovtsov <strik85@tut.by>
2023-01-17 22:20:06 +0300

    feat:add the solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+   n=n.toString(10).split('').map(int => parseInt(int, 10))
+   let temp = 0;
+   for(var i=0; i<n.length/2; i++){
+         temp=n[i];
+         n[i]=n[n.length-i-1];
+         n[n.length-1-i]=temp;
+         }
+        return n=parseInt(n.join(''),10);
 }
